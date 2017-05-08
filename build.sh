19744de8d516b67c8a9bb5adafacb6df59613e8d
skramm <sebastien.kramm@univ-rouen.fr>
2017-05-08 16:42:51 +0200

    added auto generatin of readme from sample run

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 g++ -o htcl main.cpp -std=c++11
+# run sample1
+./htcl sample1.csv
+
+cat doc_src/readme1.part > README.md
+cat sample1.csv >> README.md
+cat doc_src/readme2.part >> README.md
+cat htcl_links.html >> README.md
+cat doc_src/readme3.part >> README.md
+cat htcl_links.html >> README.md
+cat doc_src/readme4.part >> README.md
+
 
